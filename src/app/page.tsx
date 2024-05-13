105b9017a07e4027942388f89cf674ad027d634e
Mikael Marceniuk <mika.marceniuk@gmail.com>
2024-05-13 08:50:20 -0300

    feat(HomePage): Add our mission section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ import { cn } from '@/lib/utils'
 import { NextPage } from 'next'
 import Image from 'next/image'
 import { jost, mukta } from './fonts'
+import { ArrowRight, Bone, MoveRight } from 'lucide-react'
 
 const HomePage: NextPage = () => {
 	return (
@@ -45,11 +46,11 @@ const HomePage: NextPage = () => {
 					mukta.className,
 				)}
 			>
-				<div>
+				<div className='flex flex-col gap-3'>
 					<p className='text-lg font-bold tracking-widest text-gray-500'>
 						STATISTICS
 					</p>
-					<h3 className='text-2.8xl font-medium text-gray-600'>
+					<h3 className='text-2.8xl font-medium leading-8 text-gray-600'>
 						Impact at a Glance
 					</h3>
 					<h4 className={cn('text-lg text-gray-600', jost.className)}>
@@ -127,6 +128,67 @@ const HomePage: NextPage = () => {
 						</Card>
 					</li>
 				</ul>
+			</section>
+
+			<section
+				className={cn('flex flex-col gap-10 p-6 pb-10', mukta.className)}
+			>
+				<div className='flex flex-col gap-3'>
+					<p className='text-lg font-bold tracking-widest text-gray-500'>
+						OUR MISSION
+					</p>
+					<h3 className='text-2.8xl font-medium leading-8 text-gray-600'>
+						Revolutionizing Animal Rescue
+					</h3>
+					<h4 className={cn('text-lg text-gray-600', jost.className)}>
+						At Rescuify, we believe in the power of compassion to transform
+						lives. Our mission is simple yet profound: to rescue, rehabilitate,
+						and rehome animals in need, giving them the second chance they
+						deserve.
+					</h4>
+				</div>
+
+				<ul className='flex flex-col gap-4'>
+					<li className='flex gap-3 text-green-900'>
+						<div>
+							<Bone size={24} />
+						</div>
+						<p>
+							Saving lives through rescue, rehabilitation, and successful
+							adoptions.
+						</p>
+					</li>
+
+					<li className='flex gap-3 text-green-900'>
+						<div>
+							<Bone size={24} />
+						</div>
+						<p>
+							Compassionate care, transformative outcomes, and endless tails of
+							joy.
+						</p>
+					</li>
+
+					<li className='flex gap-3 text-green-900'>
+						<div>
+							<Bone size={24} />
+						</div>
+						<p>
+							Our commitment: rescuing, rehabilitating, and rehoming animals in
+							need.
+						</p>
+					</li>
+				</ul>
+
+				<div className='flex justify-between'>
+					<Button className='bg-green-800'>Learn More</Button>
+					<Button variant='outline' className='flex gap-2'>
+						Adopt a Pet{' '}
+						<div>
+							<ArrowRight size={12} />
+						</div>
+					</Button>
+				</div>
 			</section>
 		</div>
 	)
